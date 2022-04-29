a62694e12ca69828cf9e3130920deacdbd500baf
Maria Valdes <15297502@edu.cbe.ab.ca>
2022-04-29 13:13:31 -0600

    created project

---
+++ project-b/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+      <nav class="p-6 bg-[#C4C4C4]">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/products">Products</RouterLink>
+        <RouterLink to="/about-us">About Us</RouterLink>
+        <RouterLink to="/contact-us">Contact Us</RouterLink>
+        <RouterLink to="/cart">Cart</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
---
+++ project-b/src/router/index.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import ProductsView from '../views/ProductsView.vue'
+import ContactView from '../views/ContactView.vue'
+import CartView from '../views/CartView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about-us',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: ProductsView
+    },
+    {
+      path: '/contact-us',
+      name: 'contact',
+      component: ContactView
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: CartView
+    },
+  ]
+})
+
+export default router
