c16145e2dd5594866973fe00d10527b72c46ee35
Maria Valdes <15297502@edu.cbe.ab.ca>
2022-05-03 14:07:39 -0600

    added footer and fixed splash page banner

---
+++ project-b/src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="bg-[#0B0C10] text-[#C4C4C4] flex justify-between p-3">
+      
+    <div class="text-lg text-[#C4C4C4] flex flex-col pt-3">
+      <RouterLink to="/" class="px-2">Home</RouterLink>
+      <RouterLink to="/products" class="px-2">Products</RouterLink>
+      <RouterLink to="/about-us" class="px-2">About Us</RouterLink>
+      <RouterLink to="/contact-us" class="px-2">Contact Us</RouterLink>
+      <RouterLink to="/cart" class="px-2 justify-self-end">Cart</RouterLink>
+    </div>
+    <div class="text-right">
+      <p class="text-[#FC66A5] font-bold">Operating Hours</p>
+      <p>
+        Weekdays 18:00 - 22:00 <br />
+        Weekends 9:00 - 22:00
+      </p>
+      <p class="text-[#FC66A5] font-bold">Our Contacts</p>
+      <p>
+        ouremail@email.ca <br />
+        (123) 456-7890
+      </p>
+      <div class="flex justify-end">
+          <p class="text-[#FC66A5] font-bold">Check us out on social media!</p>
+          <div class="flex">
+              <img src="" alt="instagram">
+              <img src="" alt="twitter">
+              <img src="" alt="youtube">
+          </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- project-b/src/views/HomeView.vue
+++ project-b/src/views/HomeView.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="bg-[#0B0C10]">
-<p class="p-80">Hello world</p>
+  <div class="bg-[#0B0C10] pt-28 pb-36 px-16 text-[#C4C4C4] flex justify-evenly items-center">
+    <div class="border-8 border-[#FC66A5] rounded-lg box-border pl-3 pr-28 py-5 items-center">
+    <p class="py-2 pl-2 pr-28 text-[#FC66A5] font-bold text-2xl">Company Name</p>
+    <p class="px-4 pb-2">Slogan Here</p>
+    </div>
+    <p class="p-28 bg-slate-700">Image goes here</p>
   </div>
   <div class="bg-[#202833]">
-<p class="p-80">Hello world</p>
+    <p class="p-80">Hello world</p>
   </div>
+  <Footer />
 </template>
 
 <script>
+import Footer  from "../components/Footer.vue";
+
 export default {
-  
-}
+  components: {Footer}
+};
 </script>
