f23d3cf4c52a7e13f2beca8a96a0254502760eaf
Maria Valdes <15297502@edu.cbe.ab.ca>
2022-05-09 15:25:57 -0600

    tried to add firebase

---
+++ project-b/src/components/UserCreate.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <form>
+            <div>
+                <label for="name">Name</label>
+                <input type="text" v-model="form.name" required>
+            </div>
+            <div>
+                <label for="email">Email</label>
+                <input type="email" v-model="form.email" required>
+            </div>
+            <button type="submit">Create User</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import { createUser } from '@/firebase'
+import { reactive } from '@vue/reactivity'
+
+
+export default {
+    setup() {
+        const form = reactive({ name: '', email: ''})
+
+        const onSubmit = async () => {
+            await createUser({ ...form })
+            form.name = ''
+            form.email = ''
+        }
+
+        return {form, onSubmit}
+    }
+}
+</script>
--- project-b/src/views/CartView.vue
+++ project-b/src/views/CartView.vue
@@ -1,9 +1,13 @@
 <template>
-  
+  <div class="pt-20">
+      <UserCreate />
+  </div>
 </template>
 
 <script>
+import UserCreate from "../components/UserCreate.vue"
+
 export default {
-  
+  components: { UserCreate }
 }
 </script>
