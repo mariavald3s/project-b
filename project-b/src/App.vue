797fba12ae5f7da6c1c70061508644ebc9c9d8fd
Maria Valdes <15297502@edu.cbe.ab.ca>
2022-05-16 14:29:35 -0600

    hover

--- project-b/src/App.vue
+++ project-b/src/App.vue
@@ -6,10 +6,10 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
       <nav class="p-6 bg-[#0B0C10] text-[#C4C4C4] font-[32px] font-[600] flex justify-between">
         <div>
-        <RouterLink to="/" class="px-2">Home</RouterLink>
-        <RouterLink to="/products" class="px-2">Products</RouterLink>
-        <RouterLink to="/about-us" class="px-2">About Us</RouterLink>
-        <RouterLink to="/contact-us" class="px-2">Contact Us</RouterLink>
+        <RouterLink to="/" class="px-2 hover:text-[#e4e4e4]">Home</RouterLink>
+        <RouterLink to="/products" class="px-2 hover:text-[#e4e4e4]">Products</RouterLink>
+        <RouterLink to="/about-us" class="px-2 hover:text-[#e4e4e4]">About Us</RouterLink>
+        <RouterLink to="/contact-us" class="px-2 hover:text-[#e4e4e4]">Contact Us</RouterLink>
         </div>
         <div class="flex">
         <RouterLink to="/cart" class="px-2 justify-self-end">Cart</RouterLink>
